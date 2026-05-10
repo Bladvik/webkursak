37f9c79f9182167724d8ff8cfa16c65d2daf86fd
Bladvik <makspasternak123@gmail.com>
2026-05-10 21:28:03 +0300

    Фікс посилань: додав /api та правильний Vercel URL542352

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -13,9 +13,12 @@ const User = require('../models/User');
 // Налаштовуємо поштаря
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // використовуємо SSL
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    pass: process.env.EMAIL_PASS // сюди прийде App Password з Render
   }
 });
 
